10ad68344127b5c1e09d23254bbb907c51e8c8c2
oluoko <brianotieno586@gmail.com>
2024-11-11 16:53:21 +0300

    Spend Sensei V2: Header.jsx component

---
+++ src/app/_components/Header.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const Header = () => {
+  return <div></div>;
+};
+
+export default Header;
